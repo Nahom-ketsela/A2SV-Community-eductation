c327a20cbb13e672db7f643cff9331d08e590e00
Nahom ketsela <130212420+Nahom-ketsela@users.noreply.github.com>
2024-07-03 15:47:07 +0300

    Create decrypt-string-from-alphabet-to-integer-mapping.py

---
+++ decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,28 @@
+class Solution:
+    special = {
+        "10#": "j", "11#": "k", "12#": "l", "13#": "m", "14#": "n",
+        "15#": "o", "16#": "p", "17#": "q", "18#": "r", "19#": "s",
+        "20#": "t", "21#": "u", "22#": "v", "23#": "w", "24#": "x",
+        "25#": "y", "26#": "z"
+    }
+    original = {
+        "1": "a", "2": "b", "3": "c", "4": "d", "5": "e",
+        "6": "f", "7": "g", "8": "h", "9": "i"
+    }
+
+    def freqAlphabets(self, s: str) -> str:
+        
+        for key in self.special.keys():
+            s = s.replace(key, self.special[key])
+
+       
+        for key in self.original.keys():
+            s = s.replace(key, self.original[key])
+
+        return s
+
+
+
+
+
+        
